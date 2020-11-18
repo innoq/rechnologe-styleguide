f99d5e006f52c7824983c89e26bfbfe719caa614
nanoparsec <mail@nerdbabe.de>
2020-11-18 17:08:01 +0100

    Some clean up

--- components/collapsible-warning/rails_helper.rb
+++
@@ -1,6 +0,0 @@
-module ComponentHelper
-
-  def collapsible_warning_tag(open: false, &block)
-    content_tag :div, class: 'collapsible-warning', open: open &block
-  end
-end
--- components/table/squishable/rails_helper.rb
+++
@@ -1,8 +0,0 @@
-module ComponentHelper
-
-  def squishable_tag(label, short, squish_at: nil)
-    squish = squish_at || "#{label.length}ch"
-
-    content_tag :span, label, class: 'squishable', 'aria-label': label, data: {short: short}, style: "--squish-at: #{squish}"
-  end
-end
